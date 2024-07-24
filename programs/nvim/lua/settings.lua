09567a8a947f79b00ba86f11eaae5cc4b8ba4709
Ian Mancini <ianmethyst@gmail.com>
2024-07-24 15:21:13 -0300

    Add neovide settings

--- programs/nvim/lua/settings.lua
+++ programs/nvim/lua/settings.lua
@@ -31,19 +31,51 @@ vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:↴")
 vim.opt.listchars:append("tab:⟶ ")
 
+-- Neovide
+if vim.g.neovide then
+  vim.api.nvim_set_keymap("v", "<D-c>", '"+y', { noremap = true })
+  vim.api.nvim_set_keymap("v", "<sc-c>", '"+y', { noremap = true })
+
+  vim.api.nvim_set_keymap("c", "<D-v>", "<c-r>+", { noremap = true })
+  vim.api.nvim_set_keymap("c", "<sc-v>", "<c-r>+", { noremap = true })
+
+  vim.api.nvim_set_keymap("i", "<D-v>", "<c-r>+", { noremap = true })
+  vim.api.nvim_set_keymap("i", "<sc-v>", "<c-r>+", { noremap = true })
+
+  vim.api.nvim_set_keymap("t", "<D-v>", '<C-\\><C-n>"+Pi', { noremap = true })
+  vim.api.nvim_set_keymap("t", "<sc-v>", '<C-\\><C-n>"+Pi', { noremap = true })
+
+  vim.g.neovide_scale_factor = 1.0
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.25)
+  end)
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1 / 1.25)
+  end)
+
+  local padding = 16
+  vim.g.neovide_padding_top = padding
+  vim.g.neovide_padding_bottom = padding
+  vim.g.neovide_padding_right = padding
+  vim.g.neovide_padding_left = padding
+end
+
 -- Highlight on yank
 vim.cmd("au TextYankPost * lua vim.highlight.on_yank {on_visual = false}")
 
 -- Terminal
 vim.api.nvim_create_augroup("Terminal", { clear = true })
 vim.api.nvim_create_autocmd("TermOpen", {
-	group = "Terminal",
-	pattern = "*",
-	callback = function()
-		-- vim.bo.filetype = "terminal"
-		vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], { noremap = true })
-		vim.cmd("startinsert")
-	end,
+  group = "Terminal",
+  pattern = "*",
+  callback = function()
+    -- vim.bo.filetype = "terminal"
+    vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], { noremap = true })
+    vim.cmd("startinsert")
+  end,
 })
 
 vim.g.tokyonight_style = "night"
