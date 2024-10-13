f32c56da31d5b82aa7cd820c632f905d0b3b7506
Ian Mancini <ianmethyst@gmail.com>
2024-10-13 19:37:45 -0300

    Add nixos and home config for vm desktop usage

--- hosts/nixos-vm/configuration.nix
+++ hosts/nixos-vm/configuration.nix
@@ -21,14 +21,37 @@
     pulse.enable = true;
   };
 
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "discord"
+  ];
+
   environment.systemPackages = with pkgs; [
     dunst
-    kitty
     xdg-desktop-portal-hyprland
     kdePackages.polkit-kde-agent-1
+
+    wofi
+    waybar
+    pavucontrol
+    wl-clipboard
+    ungoogled-chromium
+    telegram-desktop
+    discord
+    pasystray
+    networkmanagerapplet
+    hyprpaper
+    slurp
+    grim
+    qt6ct
+  ];
+
+  fonts.packages = with pkgs; [
+    iosevka
+    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
   ];
 
   security.polkit.enable = true;
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
   programs.hyprland.enable = true;
+  services.xserver.exportConfiguration = true;
 }
-
--- hosts/nixos-vm/home.nix
+++ hosts/nixos-vm/home.nix
@@ -1,5 +1,34 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  imports = [ ../../modules/home/nixos.nix ];
+  imports = [
+    ../../modules/home/nixos.nix
+    ../../modules/home/discord/discord.nix
+    ../../modules/home/kitty/kitty.nix
+  ];
+
+  home.pointerCursor = {
+    name = "phinger-cursors-light";
+    package = pkgs.phinger-cursors;
+    size = 32;
+    gtk.enable = true;
+  };
+
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
 }
