2d22235ad0032edd5d1bc2917770720c2ab55789
Ian Mancini <ianmethyst@gmail.com>
2025-02-16 19:43:12 -0300

    Cleanup old modules

--- modules/nixos/printing.nix
+++ modules/nixos/printing.nix
@@ -6,19 +6,4 @@
     enable = true;
     nssmdns4 = true;
   };
-
-  hardware.printers = {
-    ensurePrinters = [
-      {
-        name = "HL-1212w";
-        location = "Home";
-        deviceUri = "ipp://192.168.10.66/printers/brother";
-        model = "everywhere";
-        ppdOptions = {
-          PageSize = "A4";
-        };
-      }
-    ];
-    ensureDefaultPrinter = "HL-1212w";
-  };
 }
--- modules/nixos/scanning.nix
+++ modules/nixos/scanning.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   hardware.sane = {
@@ -27,17 +27,4 @@
   };
 
   services.ipp-usb.enable = true;
-
-  environment.etc."utsushi/utsushi.conf".text = ''
-    [devices]
-    dev1.udi     = esci:networkscan://192.168.10.6:1865
-    dev1.name    = Epson DS-40 (WiFi)
-    dev1.vendor  = EPSON
-    dev1.model   = DS-40
-
-    dev2.udi     = esci:usb:04b8:0152
-    dev2.name    = Epson DS-40 (USB)
-    dev2.vendor  = EPSON
-    dev2.model   = DS-40
-  '';
 }
