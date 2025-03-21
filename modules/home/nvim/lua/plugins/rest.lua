016fd2ffc1c158bc09766b5cb3701af692d6382e
Ian Mancini <ianmethyst@gmail.com>
2025-03-21 16:10:10 -0300

    Fix rest-nvim config

--- modules/home/nvim/lua/plugins/rest.lua
+++ modules/home/nvim/lua/plugins/rest.lua
@@ -1,35 +1,31 @@
 local init = function()
-	local wk = require("which-key")
+  local wk = require("which-key")
 
-	local augroup = vim.api.nvim_create_augroup("RestNvim", {})
-	vim.api.nvim_clear_autocmds({ group = augroup })
-	vim.api.nvim_create_autocmd("BufEnter", {
-		group = augroup,
-		pattern = { "*.http" },
-		callback = function(bufnr)
-			wk.register({
-				r = {
-					"<Plug>rest run",
-					"Run the request under the cursor",
-				},
-				l = { "<Plug>rest run last", "Re-run the last request" },
-			}, {
-				mode = "n",
-				bufnr = bufnr,
-				prefix = "<localleader>",
-			})
-		end,
-	})
-end
+  local augroup = vim.api.nvim_create_augroup("RestNvim", {})
+  vim.api.nvim_clear_autocmds({ group = augroup })
+  vim.api.nvim_create_autocmd("BufEnter", {
+    group = augroup,
+    pattern = { "*.http" },
+    callback = function(ev)
+      vim.bo[ev.buf].filetype = "http"
 
-local config = function()
-	require("rest-nvim").setup({})
+      wk.add({
+        {
+          buffer = ev.buf,
+          mode = { "n" },
+          { "<localleader>r", "<cmd>Rest run<cr>",      desc = "Run the request under the cursor" },
+          { "<localleader>l", "<cmd>Rest run last<cr>", desc = "Re-run the last request" },
+        },
+      })
+    end,
+  })
 end
 
 return {
-	"rest-nvim/rest.nvim",
-	init = init,
-	config = config,
-	event = "BufEnter *.http",
-	rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+  "rest-nvim/rest.nvim",
+  init = init,
+  event = "BufEnter *.http",
+  dependencies = {
+    "j-hui/fidget.nvim",
+  },
 }
