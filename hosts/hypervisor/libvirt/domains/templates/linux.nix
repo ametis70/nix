91ea91dd6b97f66bc43fb1b41121d5ed34d41d98
Ian Mancini <ianmethyst@gmail.com>
2025-03-26 23:09:51 -0300

    Convert TrueNAS VM definition to template function

---
+++ hosts/hypervisor/libvirt/domains/templates/linux.nix
@@ -0,0 +1,248 @@
+{
+  name,
+  uuid,
+  ram ? 2,
+  cpu ? 2,
+  disk,
+  mac,
+  video ? false,
+  pci ? [ ],
+}:
+
+let
+  mkPCIHostDevs = builtins.map (dev: {
+    mode = "subsystem";
+    type = "pci";
+    managed = true;
+    driver = {
+      name = "vfio";
+    };
+    source = {
+      address = {
+        domain = dev.domain;
+        bus = dev.bus;
+        slot = dev.slot;
+        function = dev.function;
+      };
+    };
+  });
+in
+{
+  type = "kvm";
+  name = name;
+  uuid = uuid;
+  memory = {
+    count = ram;
+    unit = "GiB";
+  };
+  vcpu = {
+    placement = "static";
+    count = cpu;
+  };
+  features = {
+    acpi = { };
+    apic = { };
+  };
+  cpu = {
+    mode = "host-passthrough";
+  };
+  os = {
+    type = "hvm";
+    arch = "x86_64";
+    machine = "pc-q35-8.1";
+    loader = {
+      readonly = true;
+      type = "pflash";
+      path = "/run/libvirt/nix-ovmf/OVMF_CODE.fd";
+    };
+    nvram = {
+      template = "/run/libvirt/nix-ovmf/OVMF_VARS.fd";
+      path = "/var/lib/libvirt/qemu/nvram/${name}_VARS.fd";
+    };
+    boot = [
+      { dev = "cdrom"; }
+      { dev = "hd"; }
+    ];
+    bootmenu = {
+      enable = true;
+    };
+  };
+  clock = {
+    offset = "utc";
+    timer = [
+      {
+        name = "rtc";
+        tickpolicy = "catchup";
+      }
+      {
+        name = "pit";
+        tickpolicy = "delay";
+      }
+      {
+        name = "hpet";
+        present = false;
+      }
+    ];
+  };
+  devices =
+    {
+      emulator = "/run/libvirt/nix-emulators/qemu-system-x86_64";
+
+      disk = [
+        {
+          type = "file";
+          device = "disk";
+          driver = {
+            name = "qemu";
+            type = "qcow2";
+            cache = "none";
+            discard = "unmap";
+          };
+          source = {
+            file = "/var/lib/libvirt/images/${disk}";
+          };
+          target = {
+            dev = "vda";
+            bus = "virtio";
+          };
+        }
+      ];
+
+      interface =
+        {
+          type = "bridge";
+          model = {
+            type = "virtio";
+          };
+          source = {
+            bridge = "br0";
+          };
+        }
+        // (
+          if builtins.isString mac then
+            {
+              mac = {
+                address = mac;
+              };
+            }
+          else
+            { }
+        );
+
+      rng = {
+        model = "virtio";
+        backend = {
+          model = "random";
+          source = /dev/urandom;
+        };
+      };
+
+      hostdev = mkPCIHostDevs pci;
+
+      channel =
+        [
+          {
+            type = "unix";
+            target = {
+              type = "virtio";
+              name = "org.qemu.guest_agent.0";
+            };
+          }
+        ]
+        ++ (
+          if video then
+            [
+              {
+                type = "spicevmc";
+                target = {
+                  type = "virtio";
+                  name = "com.redhat.spice.0";
+                };
+              }
+            ]
+          else
+            [ ]
+        );
+
+      input =
+        [
+          {
+            type = "mouse";
+            bus = "ps2";
+          }
+          {
+            type = "keyboard";
+            bus = "ps2";
+          }
+        ]
+        ++ (
+          if video then
+            [
+              {
+                type = "tablet";
+                bus = "usb";
+              }
+            ]
+          else
+            [ ]
+        );
+
+    }
+    // (
+      if video then
+        {
+          sound = {
+            model = "ich9";
+          };
+
+          audio = {
+            id = 1;
+            type = "spice";
+          };
+
+          graphics = {
+            type = "spice";
+            autoport = true;
+            listen = {
+              type = "address";
+            };
+            image = {
+              compression = false;
+            };
+            gl = {
+              enable = false;
+            };
+          };
+
+          video = {
+            type = "qxl";
+            ram = 65536;
+            vram = 65536;
+            vgamem = 16384;
+            heads = 1;
+            primary = true;
+          };
+
+          redirdev = [
+            {
+              bus = "usb";
+              type = "spicevmc";
+            }
+            {
+              bus = "usb";
+              type = "spicevmc";
+            }
+            {
+              bus = "usb";
+              type = "spicevmc";
+            }
+            {
+              bus = "usb";
+              type = "spicevmc";
+            }
+          ];
+        }
+      else
+        { }
+    );
+}
--- hosts/hypervisor/libvirt/domains/truenas-scale.nix
+++ hosts/hypervisor/libvirt/domains/truenas-scale.nix
@@ -1,216 +1,32 @@
 { inputs, ... }:
+
 let
-  name = "truenas-scale";
+  template = import ./templates/linux.nix;
+  settings = {
+    name = "truenas-scale";
+    uuid = "7cb531c2-60d7-4dfc-bb1f-b4771e5c76d6";
+    ram = 16;
+    cpu = 4;
+    disk = "truenas-scale.qcow2";
+    mac = "52:54:00:7c:23:d1";
+    video = false;
+    pci = [
+      {
+        # SATA controller
+        domain = 0;
+        bus = 44; # 0x2c
+        slot = 0;
+        function = 0;
+      }
+    ];
+  };
 in
 {
   virtualisation.libvirt = {
     connections."qemu:///system" = {
       domains = [
         {
-          definition = inputs.NixVirt.lib.domain.writeXML {
-            type = "kvm";
-            name = name;
-            uuid = "7cb531c2-60d7-4dfc-bb1f-b4771e5c76d6";
-            memory = {
-              count = 16;
-              unit = "GiB";
-            };
-            vcpu = {
-              placement = "static";
-              count = 4;
-            };
-            features = {
-              acpi = { };
-              apic = { };
-            };
-            cpu = {
-              mode = "host-passthrough";
-            };
-            os = {
-              type = "hvm";
-              arch = "x86_64";
-              machine = "pc-q35-8.1";
-              loader = {
-                readonly = true;
-                type = "pflash";
-                path = "/run/libvirt/nix-ovmf/OVMF_CODE.fd";
-              };
-              nvram = {
-                template = "/run/libvirt/nix-ovmf/OVMF_VARS.fd";
-                path = "/var/lib/libvirt/qemu/nvram/${name}_VARS.fd";
-              };
-              boot = [
-                { dev = "cdrom"; }
-                { dev = "hd"; }
-              ];
-              bootmenu = {
-                enable = true;
-              };
-            };
-            clock = {
-              offset = "utc";
-              timer = [
-                {
-                  name = "rtc";
-                  tickpolicy = "catchup";
-                }
-                {
-                  name = "pit";
-                  tickpolicy = "delay";
-                }
-                {
-                  name = "hpet";
-                  present = false;
-                }
-              ];
-            };
-            devices = {
-              emulator = "/run/libvirt/nix-emulators/qemu-system-x86_64";
-
-              disk = [
-                {
-                  type = "file";
-                  device = "disk";
-                  driver = {
-                    name = "qemu";
-                    type = "qcow2";
-                    cache = "none";
-                    discard = "unmap";
-                  };
-                  source = {
-                    file = "/var/lib/libvirt/images/truenas-scale.qcow2";
-                  };
-                  target = {
-                    dev = "vda";
-                    bus = "virtio";
-                  };
-                }
-              ];
-
-              interface = {
-                type = "bridge";
-                mac = {
-                  address = "52:54:00:7c:23:d1";
-                };
-                model = {
-                  type = "virtio";
-                };
-                source = {
-                  bridge = "br0";
-                };
-              };
-
-              channel = [
-                {
-                  type = "spicevmc";
-                  target = {
-                    type = "virtio";
-                    name = "com.redhat.spice.0";
-                  };
-                }
-                {
-                  type = "unix";
-                  target = {
-                    type = "virtio";
-                    name = "org.qemu.guest_agent.0";
-                  };
-                }
-              ];
-
-              input = [
-                {
-                  type = "tablet";
-                  bus = "usb";
-                }
-                {
-                  type = "mouse";
-                  bus = "ps2";
-                }
-                {
-                  type = "keyboard";
-                  bus = "ps2";
-                }
-              ];
-
-              sound = {
-                model = "ich9";
-              };
-
-              audio = {
-                id = 1;
-                type = "spice";
-              };
-
-              graphics = {
-                type = "spice";
-                autoport = true;
-                listen = {
-                  type = "address";
-                };
-                image = {
-                  compression = false;
-                };
-                gl = {
-                  enable = false;
-                };
-              };
-
-              video = {
-                type = "qxl";
-                ram = 65536;
-                vram = 65536;
-                vgamem = 16384;
-                heads = 1;
-                primary = true;
-              };
-
-              rng = {
-                model = "virtio";
-                backend = {
-                  model = "random";
-                  source = /dev/urandom;
-                };
-              };
-
-              hostdev = [
-                {
-                  mode = "subsystem";
-                  type = "pci";
-                  managed = true;
-                  driver = {
-                    name = "vfio";
-                  };
-                  source = {
-                    address = {
-                      domain = 0;
-                      bus = 44; # 0x2c
-                      slot = 0;
-                      function = 0;
-                    };
-                  };
-                }
-              ];
-
-              redirdev = [
-                {
-                  bus = "usb";
-                  type = "spicevmc";
-                }
-                {
-                  bus = "usb";
-                  type = "spicevmc";
-                }
-                {
-                  bus = "usb";
-                  type = "spicevmc";
-                }
-                {
-                  bus = "usb";
-                  type = "spicevmc";
-                }
-              ];
-            };
-          };
+          definition = inputs.NixVirt.lib.domain.writeXML (template settings);
           active = true;
         }
       ];
