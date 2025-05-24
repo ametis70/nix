6e685289e22f1347ba8722da24e17077f2011629
Ian Mancini <ianmethyst@gmail.com>
2025-05-24 03:33:12 -0300

    Fix Windows SSH config

--- hosts/windows10/home.nix
+++ hosts/windows10/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
 {
   imports = [
@@ -7,7 +7,36 @@
     ../../modules/home/gpg-agent/gpg-agent.nix
   ];
 
-  services.gpg-agent.pinentryPackage = pkgs.pinentry-curses;
+  services.gpg-agent = {
+    pinentryPackage = pkgs.pinentry-curses;
+  };
+
+  programs.keychain = {
+    enable = true;
+    agents = [
+      "ssh"
+      "gpg"
+    ];
+    keys = [
+      "id_ed25519"
+      "92D7C9EB0B70F7B3"
+    ];
+    enableZshIntegration = true;
+    extraFlags = [
+      "--noask"
+      "--quiet"
+    ];
+  };
+
+  programs.ssh = {
+    enable = true;
+    addKeysToAgent = "yes";
+  };
+
+  home.packages = [
+    pkgs.virt-manager
+    inputs.nixgl.packages.x86_64-linux.nixGLIntel
+  ];
 
   home.stateVersion = "24.11";
 }
--- modules/home/gpg-agent/gpg-agent.nix
+++ modules/home/gpg-agent/gpg-agent.nix
@@ -4,7 +4,8 @@
   services.gpg-agent = {
     enable = true;
     enableScDaemon = false;
-    enableZshIntegration = false;
+    enableZshIntegration = lib.mkDefault false;
+    enableSshSupport = false;
     pinentryPackage = lib.mkDefault pkgs.pinentry-gnome3;
   };
 }
