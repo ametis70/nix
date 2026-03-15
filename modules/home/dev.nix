37450ba2e42cb188144d9b0a2c96d3554af78a80
Ian Mancini <ianmethyst@gmail.com>
2026-03-15 13:27:11 -0300

    Use opencode home manager module and enable mcps

--- modules/home/dev.nix
+++ modules/home/dev.nix
@@ -12,6 +12,36 @@
     go
     python3
     codex
-    opencode
   ];
+
+  home.sessionVariables = {
+    OPENCODE_EXPERIMENTAL_LSP_TOOL = "true";
+    OPENCODE_DISABLE_LSP_DOWNLOAD = "true";
+  };
+
+  programs = {
+    opencode = {
+      enable = true;
+      enableMcpIntegration = true;
+      settings = {
+        permission = {
+          lsp = "allow";
+        };
+      };
+    };
+    mcp = {
+      enable = true;
+      servers = {
+        exa = {
+          url = "https://mcp.exa.ai/mcp";
+        };
+        context7 = {
+          url = "https://mcp.context7.com/mcp";
+        };
+        grep-app = {
+          url = "https://mcp.grep.app";
+        };
+      };
+    };
+  };
 }
